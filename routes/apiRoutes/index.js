bb868c9daca4126735ed889d774a847f41d8bfad
erockenmeyer <erikrockenmeyer@gmail.com>
2022-04-28 23:36:59 -0500

    finish html routes and start api routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,31 @@
+// npm modules & other variables
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const notes = require('../../db/db.json');
+
+// base api/get all notes
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+// add new note
+router.post('/notes', (req, res) => {
+    const note = req.body;
+
+    // set unique id for note
+
+    // check that there is actually a note (title & text)
+    if (!note.title || !note.text) {
+        res.status(400).send('Please add something to your note before saving.');
+    } else {
+        notes.push(note);
+        fs.writeFileSync(
+            path.join(__dirname, '../../db/db.json'),
+            JSON.stringify({ notes }, null, 2)
+        );
+        res.json(note);
+    }
+})
+
+module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,7 +1,20 @@
 // npm modules
 const router = require('express').Router();
+const path = require('path');
 
 // landing page
 router.get('/', (req, res) => {
-    res.sendFile('../../public/index.html');
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
+
+// notes view 
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+// anything else routes back to landing
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+})
+
+module.exports = router;
